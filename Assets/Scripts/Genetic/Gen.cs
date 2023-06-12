7edf3ce3d7cdb6cb9eacfdc9f7e6d25958bc1514
Ilia-c <3961193@gmail.com>
2023-06-12 19:36:53 +0300

    Ver_0.3

--- Assets/Scripts/Genetic/Gen.cs
+++ Assets/Scripts/Genetic/Gen.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Gen : MonoBehaviour
 {
+    public Fuzzy_logic logic;
     // Описывает гены конкретного робота
+
+    public List<float> Gen_vect = new List<float>(); // Позиции точек препятствий
+
+    public void generate_genetic()
+    {
+        for (int i = 0; i < logic.Speed_func.Length; i++)
+        {
+            for(int j = 0; j<4; j++)
+            {
+                Gen_vect.Add(logic.Speed_func[i, j]);
+            }
+
+        }
+
+
+    }
+
 }
